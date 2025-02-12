5af67aa7b022fae14013eacd0eee38baba7213b3
acreedcyber <119901525+acreedcyber@users.noreply.github.com>
2025-02-11 20:03:23 -0600

    Update graphics.c

--- graphics.c
+++ graphics.c
@@ -1,126 +1,53 @@
-#include "graphics.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
+#include "graphics.h"
 
-// SDL variables
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
+// Load UI texture from ICSim's ic.svg
+SDL_Texture *ic_texture;
 
-// Initialize SDL graphics
-int init_sdl_graphics() {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL Initialization failed: %s\n", SDL_GetError());
-        return -1;
+SDL_Texture *load_texture(SDL_Renderer *renderer, const char *file) {
+    SDL_Surface *image = IMG_Load(file);
+    if (!image) {
+        printf("Failed to load image %s: %s\n", file, IMG_GetError());
+        return NULL;
     }
-    window = SDL_CreateWindow("Honda ICSim Dashboard", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-    if (!window) {
-        printf("Window creation failed: %s\n", SDL_GetError());
-        return -1;
-    }
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        printf("Renderer creation failed: %s\n", SDL_GetError());
-        return -1;
-    }
-    return 0;
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
+    SDL_FreeSurface(image);
+    return texture;
 }
 
-// Clear screen
-void sdl_clear_screen() {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+void init_ui(SDL_Renderer *renderer) {
+    ic_texture = load_texture(renderer, "assets/ic.svg");  // Use ICSim UI
+}
+
+void draw_texture(SDL_Renderer *renderer, SDL_Texture *texture, int x, int y, int w, int h) {
+    SDL_Rect dest = { x, y, w, h };
+    SDL_RenderCopy(renderer, texture, NULL, &dest);
+}
+
+void draw_rotated_texture(SDL_Renderer *renderer, SDL_Texture *texture, int x, int y, double angle) {
+    SDL_Rect dest = { x, y, 200, 200 };
+    SDL_RenderCopyEx(renderer, texture, NULL, &dest, angle, NULL, SDL_FLIP_NONE);
+}
+
+void draw_dashboard(SDL_Renderer *renderer, VehicleState *car) {
+    // Render ICSim-style dashboard
+    draw_texture(renderer, ic_texture, 50, 50, 600, 400);  // Full ICSim UI
+    
+    // Animate speedometer needle
+    draw_rotated_texture(renderer, ic_texture, 250, 180, car->speed * 1.5);
+
+    // Animate steering wheel movement from ic.svg
+    draw_rotated_texture(renderer, ic_texture, 400, 300, car->steering * 2);
+
+    // Show doors status from ic.svg
+    if (car->doors)
+        draw_texture(renderer, ic_texture, 500, 350, 50, 50);
+}
+
+void update_dashboard(SDL_Renderer *renderer, VehicleState *car) {
     SDL_RenderClear(renderer);
-}
-
-// Refresh screen
-void sdl_refresh_screen() {
+    draw_dashboard(renderer, car);
     SDL_RenderPresent(renderer);
-}
-
-// Draw speedometer
-void draw_speedometer_sdl(int speed) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_Rect speed_rect = {50, 50, speed * 2, 20};
-    SDL_RenderFillRect(renderer, &speed_rect);
-}
-
-// Draw RPM gauge
-void draw_rpm_gauge_sdl(int rpm) {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_Rect rpm_rect = {50, 100, rpm / 100, 20};
-    SDL_RenderFillRect(renderer, &rpm_rect);
-}
-
-// Draw steering wheel status
-void draw_steering_wheel_sdl(int angle) {
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-    SDL_Rect steer_rect = {50, 150, angle * 2, 20};
-    SDL_RenderFillRect(renderer, &steer_rect);
-}
-
-// Draw brake indicator
-void draw_brake_indicator_sdl(int status) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-    if (status) {
-        SDL_Rect brake_rect = {50, 200, 50, 20};
-        SDL_RenderFillRect(renderer, &brake_rect);
-    }
-}
-
-// Draw lights indicator
-void draw_light_status_sdl(int status) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-    if (status) {
-        SDL_Rect light_rect = {50, 250, 50, 20};
-        SDL_RenderFillRect(renderer, &light_rect);
-    }
-}
-
-// Draw door status
-void draw_door_status_sdl(int status) {
-    SDL_SetRenderDrawColor(renderer, 255, 165, 0, 255);
-    if (status) {
-        SDL_Rect door_rect = {50, 300, 50, 20};
-        SDL_RenderFillRect(renderer, &door_rect);
-    }
-}
-
-// Draw infotainment status
-void draw_infotainment_status_sdl(int status) {
-    SDL_SetRenderDrawColor(renderer, 75, 0, 130, 255);
-    if (status) {
-        SDL_Rect info_rect = {50, 350, 50, 20};
-        SDL_RenderFillRect(renderer, &info_rect);
-    }
-}
-
-// Draw airbag status
-void draw_airbag_status_sdl(int status) {
-    SDL_SetRenderDrawColor(renderer, 255, 69, 0, 255);
-    if (status) {
-        SDL_Rect airbag_rect = {50, 400, 50, 20};
-        SDL_RenderFillRect(renderer, &airbag_rect);
-    }
-}
-
-// Draw tire pressure status
-void draw_tire_pressure_sdl(int pressure) {
-    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-    SDL_Rect tire_rect = {50, 450, pressure, 20};
-    SDL_RenderFillRect(renderer, &tire_rect);
-}
-
-// Draw engine temperature
-void draw_engine_temp_sdl(int temp) {
-    SDL_SetRenderDrawColor(renderer, 139, 0, 0, 255);
-    SDL_Rect temp_rect = {50, 500, temp, 20};
-    SDL_RenderFillRect(renderer, &temp_rect);
-}
-
-// Draw fuel level
-void draw_fuel_level_sdl(int fuel) {
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-    SDL_Rect fuel_rect = {50, 550, fuel * 2, 20};
-    SDL_RenderFillRect(renderer, &fuel_rect);
 }

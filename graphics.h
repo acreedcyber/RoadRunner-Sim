46d18a0a39cc08174f12e829f06321f2b4666c2d
acreedcyber <119901525+acreedcyber@users.noreply.github.com>
2025-02-11 20:39:42 -0600

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -2,48 +2,12 @@
 #define GRAPHICS_H
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#include "vehicle_state.h"
 
-// Initialize SDL graphics
+extern SDL_Renderer *renderer;
+
 void init_sdl_graphics();
+void update_dashboard(SDL_Renderer *renderer, VehicleState *car);
+void close_sdl_graphics();
 
-// Clear the screen
-void sdl_clear_screen();
-
-// Refresh the screen
-void sdl_refresh_screen();
-
-// Draw speedometer
-void draw_speedometer_sdl(int speed);
-
-// Draw RPM gauge
-void draw_rpm_gauge_sdl(int rpm);
-
-// Draw steering wheel status
-void draw_steering_wheel_sdl(int angle);
-
-// Draw brake indicator
-void draw_brake_indicator_sdl(int status);
-
-// Draw lights indicator
-void draw_light_status_sdl(int status);
-
-// Draw door status
-void draw_door_status_sdl(int status);
-
-// Draw infotainment status
-void draw_infotainment_status_sdl(int status);
-
-// Draw airbag status
-void draw_airbag_status_sdl(int status);
-
-// Draw tire pressure status
-void draw_tire_pressure_sdl(int pressure);
-
-// Draw engine temperature
-void draw_engine_temp_sdl(int temp);
-
-// Draw fuel level
-void draw_fuel_level_sdl(int fuel);
-
-#endif // GRAPHICS_H
+#endif

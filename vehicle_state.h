4d9a6d2912d11850a9360bacf28742c4c925fd1e
acreedcyber <119901525+acreedcyber@users.noreply.github.com>
2025-02-11 20:35:17 -0600

    Update vehicle_state.h

--- vehicle_state.h
+++ vehicle_state.h
@@ -15,4 +15,17 @@ typedef struct {
     int fuel_level;
 } VehicleState;
 
-#endif // VEHICLE_STATE_H
+// Define Honda CAN IDs
+#define HONDA_SPEED_ID 0x158
+#define HONDA_RPM_ID 0x17C
+#define HONDA_STEERING_ID 0x123
+#define HONDA_BRAKE_ID 0x1FA
+#define HONDA_LIGHTS_ID 0x3D0
+#define HONDA_DOOR_ID 0x3E9
+#define HONDA_INFOTAINMENT_ID 0x42A
+#define HONDA_AIRBAG_ID 0x500
+#define HONDA_TIRE_PRESSURE_ID 0x5A0
+#define HONDA_ENGINE_TEMP_ID 0x610
+#define HONDA_FUEL_LEVEL_ID 0x620
+
+#endif

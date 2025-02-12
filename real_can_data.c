7180608c9edfb94274399e5cd12c16f3ee69ee84
acreedcyber <119901525+acreedcyber@users.noreply.github.com>
2025-02-11 19:06:51 -0600

    Update real_can_data.c

--- real_can_data.c
+++ real_can_data.c
@@ -1,42 +1,36 @@
-#ifndef REAL_CAN_DATA_H
-#define REAL_CAN_DATA_H
-
-#include <linux/can.h>
-#include "honda_icsim.h"
+#include "real_can_data.h"
 #include <stdio.h>
 #include <string.h>
 
-// Function to parse real Honda CAN data
 void parse_real_honda_can_data(struct can_frame *frame, VehicleState *car) {
     switch (frame->can_id) {
         case 0x158: // Speed
             car->speed = frame->data[0];
-            printf("Speed updated: %d mph\n", car->speed);
+            printf("Speed updated: %d mph\\n", car->speed);
             break;
         case 0x17C: // RPM
             car->rpm = frame->data[0] * 100;
-            printf("RPM updated: %d\n", car->rpm);
+            printf("RPM updated: %d\\n", car->rpm);
             break;
         case 0x123: // Steering
             car->steering = frame->data[0] - 30;
-            printf("Steering angle: %d\n", car->steering);
+            printf("Steering angle: %d\\n", car->steering);
             break;
         case 0x1FA: // Brakes
             car->brakes = frame->data[0];
-            printf("Brakes: %s\n", car->brakes ? "Engaged" : "Released");
+            printf("Brakes: %s\\n", car->brakes ? "Engaged" : "Released");
             break;
         case 0x3D0: // Lights
             car->lights = frame->data[0];
-            printf("Lights: %s\n", car->lights ? "On" : "Off");
+            printf("Lights: %s\\n", car->lights ? "On" : "Off");
             break;
         case 0x3E9: // Doors
             car->doors = frame->data[0];
-            printf("Doors: %s\n", car->doors ? "Locked" : "Unlocked");
+            printf("Doors: %s\\n", car->doors ? "Locked" : "Unlocked");
             break;
         default:
-            printf("Unknown CAN ID: 0x%X\n", frame->can_id);
+            printf("Unknown CAN ID: 0x%X\\n", frame->can_id);
             break;
     }
 }
 
-#endif // REAL_CAN_DATA_H

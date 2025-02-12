a5471068b04ca44a6dbc612c6196e09325e27b24
acreedcyber <119901525+acreedcyber@users.noreply.github.com>
2025-02-11 19:06:23 -0600

    Update real_can_data.h

--- real_can_data.h
+++ real_can_data.h
@@ -2,7 +2,7 @@
 #define REAL_CAN_DATA_H
 
 #include <linux/can.h>
-#include "honda_icsim.h"
+#include "vehicle_state.h"  // Include the struct definition
 
 // Function to parse real Honda CAN data
 void parse_real_honda_can_data(struct can_frame *frame, VehicleState *car);
